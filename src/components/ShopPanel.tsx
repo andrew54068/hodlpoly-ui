5386f077489d683d5fac65e362399c21cdce2ff3
andrew54068 <andrew0424718012@yahoo.com.tw>
2024-02-16 16:12:36 +0800

    feat: be able to buy props.

--- src/components/ShopPanel.tsx
+++ src/components/ShopPanel.tsx
@@ -1,25 +1,77 @@
 import { Flex, SimpleGrid, TabPanel } from "@chakra-ui/react";
-import { ShopItem } from "./GameMenu";
-import { useState } from "react";
+import { Props, ShopItem } from "./GameMenu";
+import fomopolyAbi from "src/abi/fomopoly";
+import { useCallback, useState } from "react";
 import { PropButton } from "./PropButton";
 import { SelectedPropCard } from "./SelectedPropCard";
+import { useAccount, useReadContract } from "wagmi";
+import { getConnectedWalletClient, publicClient } from "src/config/clients";
+import { getContract } from "viem";
+import { FOMOPOLY_ADDRESS_TESTNET } from "src/constants";
 
 interface ShopPanelProps {
   items: ShopItem[];
+  onUpdateAmount: () => void;
 }
 
-export const ShopPanel = ({ items }: ShopPanelProps) => {
+export const ShopPanel = ({ items, onUpdateAmount }: ShopPanelProps) => {
   const [selectedItem, setSelectedItem] = useState(items[0]);
+  const { address = "" } = useAccount();
+
+  const getContractClient = useCallback(async () => {
+    const walletClient = await getConnectedWalletClient({ address });
+
+    if (!walletClient) return;
+    const contract = getContract({
+      address: FOMOPOLY_ADDRESS_TESTNET,
+      abi: fomopolyAbi.abi,
+      client: { public: publicClient, wallet: walletClient },
+    });
+
+    return contract;
+  }, [address]);
+
+  console.log(`💥 Props.OddDice: ${JSON.stringify(Props.OddDice, null, "  ")}`);
+
+  const { data: allPropsPrices = [] } = useReadContract({
+    abi: fomopolyAbi.abi,
+    address: FOMOPOLY_ADDRESS_TESTNET,
+    functionName: "getPropPricesETH",
+    args: [],
+  });
+
+  const onClickBuyProp = async (item: ShopItem) => {
+    const contract = await getContractClient();
+    if (!contract) return;
+    const propPrice = allPropsPrices[item.prop];
+    console.log("propPrice :", propPrice);
+
+    const hash = await contract.write.buyProps([item.prop], {
+      value: propPrice,
+    });
+    console.log("hash :", hash);
+    onUpdateAmount()
+  };
 
   return (
     <TabPanel p="24px">
       <Flex alignItems="start" justifyContent="space-between" columnGap="24px">
         <SimpleGrid columns={3} spacing="20px" m="0px">
           {items.map((item) => {
-            return <PropButton key={item.name} item={item} onClickItem={setSelectedItem} />;
+            return (
+              <PropButton
+                key={item.name}
+                item={item}
+                onClickItem={setSelectedItem}
+              />
+            );
           })}
         </SimpleGrid>
-        <SelectedPropCard item={selectedItem} />
+        <SelectedPropCard
+          actionTitle="Buy"
+          item={selectedItem}
+          onClickActionItem={onClickBuyProp}
+        />
       </Flex>
     </TabPanel>
   );
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,10 @@
-import { ShopItem } from "src/components/GameMenu";
+import { Props, ShopItem } from "src/components/GameMenu";
 import Protector from "src/assets/protector.svg?react";
 // import WorldWideTraveler from "src/assets/worldWideTraveler.svg?react";
-import OddDise from "src/assets/oddDise.svg?react";
-import HighDise from "src/assets/highDise.svg?react";
-import EvenDise from "src/assets/evenDise.svg?react";
-import LowDise from "src/assets/lowDise.svg?react";
+import OddDice from "src/assets/oddDice.svg?react";
+import HighDice from "src/assets/highDice.svg?react";
+import EvenDice from "src/assets/evenDice.svg?react";
+import LowDice from "src/assets/lowDice.svg?react";
 
 export const NAVBAR_HIGHT = 75
 
@@ -13,6 +13,7 @@ export const shopItems: ShopItem[] = [
         image: Protector,
         name: "Land Protector",
         desc: "Increases the price of a specified owned land by 200%, reducing the likelihood of other players purchasing it.",
+        prop: Props.TitleDeed
     },
     // {
     //     image: WorldWideTraveler,
@@ -20,28 +21,33 @@ export const shopItems: ShopItem[] = [
     //     desc: "Instantly travel to a specified land on the board.",
     // },
     {
-        image: HighDise,
+        image: HighDice,
         name: "Special Dice / High",
         desc: "Rolls higher numbers (4,5,6) to navigate you closer to your target land.",
+        prop: Props.HighDice
     },
     {
-        image: LowDise,
+        image: LowDice,
         name: "Special Dice / Low",
         desc: "Rolls lower numbers (1,2,3) to navigate you closer to your target land.",
+        prop: Props.LowDice
     },
     {
-        image: OddDise,
+        image: OddDice,
         name: "Special Dice / Odd",
         desc: "Rolls odd numbers (1,3,5) to navigate you closer to your target land.",
+        prop: Props.OddDice
     },
     {
-        image: EvenDise,
+        image: EvenDice,
         name: "Special Dice / Even",
         desc: "Rolls even numbers (2,4,6) to navigate you closer to your target land.",
+        prop: Props.EvenDice
     },
     {
-        image: EvenDise,
+        image: EvenDice,
         name: "Ticket",
         desc: "Ticket to treasure! Increase your chance with every ticket for the prize pool.",
+        prop: Props.EvenDice
     },
 ];
