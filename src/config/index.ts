40b77a489d80eb3b18498222c98729b71cb55bd2
andrew54068 <andrew0424718012@yahoo.com.tw>
2024-02-21 13:51:30 +0800

    feat: add blocto app id.

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,7 @@ import { http } from "wagmi";
 import { injected } from "wagmi/connectors";
 import { blocto } from "@blocto/wagmi-connector";
 
+const appId = import.meta.env.VITE_APP_BLOCTO_APP_ID;
 
 export const walletConnectProjectId = '7a453bf7754ad57224095646ba5f2b93'
 export const supportChains = [goerli]
@@ -11,7 +12,7 @@ export const supportChains = [goerli]
 export const wagmiConfig = createConfig({
   chains: [goerli, mainnet],
   multiInjectedProviderDiscovery: false,
-  connectors: [blocto(), injected()],
+  connectors: [blocto({ appId }), injected()],
   ssr: true,
   transports: {
     [goerli.id]: http(),
