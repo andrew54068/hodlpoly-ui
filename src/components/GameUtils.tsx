71fc6c3c981c4c341fe35b33de21e4afca1edc4c
moojing <starts789@gmail.com>
2024-02-22 18:29:04 +0800

    fix: zoom in and zoom out problem.

---
+++ src/components/GameUtils.tsx
@@ -0,0 +1,156 @@
+import { Button, Switch, Image, Flex, Text } from "@chakra-ui/react";
+import dice from "src/assets/dice.png";
+import GameMenu from "./GameMenu";
+import { NAVBAR_HIGHT } from "src/utils/constants";
+import { BalanceBoard } from "./BalanceBoard";
+import { UserProfile } from "./UserProfile";
+import { LeaderBoard } from "./LeaderBoard";
+import useUserActions from "src/hooks/useUserActions";
+import { NumberType } from "src/types";
+
+export default function GameUtils({
+  outterSharedMargin,
+  onHeatMapSwitchClick,
+  isHeatMapMode,
+  hideAllOptions,
+}: {
+  outterSharedMargin: number;
+  onHeatMapSwitchClick: () => void;
+  isHeatMapMode: boolean;
+  hideAllOptions: boolean;
+}) {
+  const { rollTheDice, buyLand } = useUserActions();
+
+  const onClickMove = async () => {
+    await rollTheDice(NumberType.Any);
+  };
+
+  const onClickBuyLand = async () => {
+    const hash = await buyLand();
+    console.log("hash :", hash);
+  };
+
+  return (
+    <>
+      {!isHeatMapMode && !hideAllOptions && (
+        <>
+          <Flex
+            position="absolute"
+            w="100%"
+            top={`${outterSharedMargin + NAVBAR_HIGHT}px`}
+            justifyContent="space-between"
+            alignItems="start"
+            p="0px 52px"
+          >
+            <UserProfile />
+            <LeaderBoard width="700px" height="52px" justifyContent="center" />
+            <BalanceBoard width="180px" height="52px" />
+          </Flex>
+
+          <GameMenu
+            position="absolute"
+            width="60px"
+            height="204px"
+            right={`${outterSharedMargin}px`}
+            top={`${156 + NAVBAR_HIGHT}px`}
+            p="0px"
+            m="0px"
+          />
+
+          <Button
+            width="146px"
+            height="146px"
+            position="absolute"
+            bottom={`${outterSharedMargin + 146 + 30}px`}
+            right={`${outterSharedMargin}px`}
+            onClick={onClickBuyLand}
+            _hover={{
+              background:
+                "0px 25px 60px -15px rgba(3, 3, 3, 0.20), 0px 25px 60px -15px rgba(3, 3, 3, 0.12), 0px 0px 7px 0px #222",
+              boxShadow:
+                "0px 25px 60px -15px rgba(16, 24, 40, 0.20), 0px 25px 60px -15px rgba(16, 24, 40, 0.12), 0px 0px 7px 0px #222",
+              linearGradient: "(270deg, #FFF -26.8%, #000 30.45%)",
+            }}
+            _active={{
+              background: "linear-gradient(270deg, #FFF -26.8%, #000 30.45%)",
+              boxShadow:
+                "0px 25px 60px -15px rgba(16, 24, 40, 0.20), 0px 25px 60px -15px rgba(16, 24, 40, 0.12), -6px 1px 0px 0px rgba(0, 0, 0, 0.40) inset",
+              transform: "scale(0.98)",
+            }}
+            boxShadow="0px 25px 60px -15px rgba(16, 24, 40, 0.20), 0px 25px 60px -15px rgba(16, 24, 40, 0.12)"
+            background="linear-gradient(270deg, #FFF -26.8%, #000 30.45%)"
+            borderRadius="16.22px"
+          >
+            <Flex direction="column" alignItems="center">
+              <Image src={dice}></Image>
+              <Text
+                color="#FFFFFF"
+                textAlign="center"
+                fontFamily="Inter"
+                fontSize="26px"
+                fontStyle="normal"
+                fontWeight="500"
+                lineHeight="35.689px"
+                letterSpacing="-0.72px"
+              >
+                Buy Land!
+              </Text>
+            </Flex>
+          </Button>
+
+          <Button
+            width="146px"
+            height="146px"
+            position="absolute"
+            bottom={`${outterSharedMargin}px`}
+            right={`${outterSharedMargin}px`}
+            onClick={onClickMove}
+            _hover={{
+              background:
+                "0px 25px 60px -15px rgba(3, 3, 3, 0.20), 0px 25px 60px -15px rgba(3, 3, 3, 0.12), 0px 0px 7px 0px #222",
+              boxShadow:
+                "0px 25px 60px -15px rgba(16, 24, 40, 0.20), 0px 25px 60px -15px rgba(16, 24, 40, 0.12), 0px 0px 7px 0px #222",
+              linearGradient: "(270deg, #FFF -26.8%, #000 30.45%)",
+            }}
+            _active={{
+              background: "linear-gradient(270deg, #FFF -26.8%, #000 30.45%)",
+              boxShadow:
+                "0px 25px 60px -15px rgba(16, 24, 40, 0.20), 0px 25px 60px -15px rgba(16, 24, 40, 0.12), -6px 1px 0px 0px rgba(0, 0, 0, 0.40) inset",
+              transform: "scale(0.98)",
+            }}
+            boxShadow="0px 25px 60px -15px rgba(16, 24, 40, 0.20), 0px 25px 60px -15px rgba(16, 24, 40, 0.12)"
+            background="linear-gradient(270deg, #FFF -26.8%, #000 30.45%)"
+            borderRadius="16.22px"
+          >
+            <Flex direction="column" alignItems="center">
+              <Image src={dice}></Image>
+              <Text
+                color="#FFFFFF"
+                textAlign="center"
+                fontFamily="Inter"
+                fontSize="36px"
+                fontStyle="normal"
+                fontWeight="500"
+                lineHeight="35.689px"
+                letterSpacing="-0.72px"
+              >
+                GO!
+              </Text>
+            </Flex>
+          </Button>
+        </>
+      )}
+
+      {!hideAllOptions && (
+        <Switch
+          position="absolute"
+          left={`${outterSharedMargin}px`}
+          bottom={`${outterSharedMargin}px`}
+          size="lg"
+          colorScheme="red"
+          onChange={onHeatMapSwitchClick}
+        />
+      )}
+    </>
+  );
+}
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,25 +1,18 @@
 import { useRef, useEffect, useState, useContext } from "react";
-import { Box, Button, Switch, Image, Flex, Text } from "@chakra-ui/react";
+import { Box, Button, Flex } from "@chakra-ui/react";
 import "src/utils/phaser";
 import { config } from "src/utils/phaser";
 import { GlobalContext } from "src/context/global";
-import { ConnectModalProvider } from "src/components/WalletConnectModal";
-import { NAVBAR_HIGHT } from "src/utils/constants";
-import useUserFomopolyData from "src/hooks/useUserFomopolyData";
-import useUserActions from "src/hooks/useUserActions";
-import GameMenu from "./GameMenu";
-import { NumberType } from "src/types";
+import GameUtils from "./GameUtils";
 import getHeatMapColors from "src/utils/getHeatMapColors";
-import dice from "src/assets/dice.png";
-import { BalanceBoard } from "./BalanceBoard";
-import { UserProfile } from "./UserProfile";
-import { LeaderBoard } from "./LeaderBoard";
+import { ConnectModalProvider } from "src/components/WalletConnectModal";
+import useUserFomopolyData from "src/hooks/useUserFomopolyData";
 
 export const outterSharedMargin = 54;
 
 export default function MainPage() {
-  const hasInit = useRef(false);
   const [isHeatMapMode, setIsHeatMapMode] = useState(false);
+  const hasInit = useRef(false);
 
   const {
     isConnectModalOpen,
@@ -28,15 +21,8 @@ export default function MainPage() {
     setSelectingLandPurpose,
   } = useContext(GlobalContext);
 
-  const {
-    allLandPrices,
-    refetchAllLandPrices,
-    landAmount,
-    userOwnedLands,
-    refetchUserOwnedLands,
-    userSteps,
-  } = useUserFomopolyData();
-  const { rollTheDice, buyLand } = useUserActions();
+  const { allLandPrices, landAmount, userOwnedLands, userSteps } =
+    useUserFomopolyData();
 
   useEffect(() => {
     const phaserContainer = document.getElementById("phaser-zone-fomopoly");
@@ -73,17 +59,6 @@ export default function MainPage() {
       setIsHeatMapMode((prev) => !prev);
       window.fomopolyMap.setHeatMapMode(!isHeatMapMode, heatMapSteps);
     }
-    refetchAllLandPrices();
-  };
-
-  const onClickMove = async () => {
-    await rollTheDice(NumberType.Any);
-  };
-
-  const onClickBuyLand = async () => {
-    const hash = await buyLand();
-    console.log("hash :", hash);
-    refetchUserOwnedLands();
   };
 
   const shouldHideOptions = selectingLandPurpose != null;
@@ -93,17 +68,18 @@ export default function MainPage() {
       isOpen={isConnectModalOpen}
       onClose={onConnectModalClose}
     >
-      <Box minH="100vh" pt="75px">
+      <Box minH="100vsh" pt="75px">
         <Box id="phaser-zone-fomopoly" position="relative">
-          <Box
-            position="absolute"
-            bottom="20px"
-            left="20px"
-            color="white"
-          ></Box>
+          <GameUtils
+            isHeatMapMode={isHeatMapMode}
+            hideAllOptions={shouldHideOptions}
+            outterSharedMargin={outterSharedMargin}
+            onHeatMapSwitchClick={onHeatMapSwitchClick}
+          />
         </Box>
       </Box>
-      {shouldHideOptions ? (
+      //@todo: selection mode
+      {shouldHideOptions && (
         <Flex
           position="absolute"
           width="100%"
@@ -118,130 +94,6 @@ export default function MainPage() {
             Cancel
           </Button>
         </Flex>
-      ) : (
-        <Box position="absolute" w="100%" h="100%" top="0px">
-          {!isHeatMapMode && (
-            <Flex
-              position="absolute"
-              w="100%"
-              top={`${outterSharedMargin + NAVBAR_HIGHT}px`}
-              justifyContent="space-between"
-              alignItems="start"
-              p="0px 52px"
-            >
-              <UserProfile />
-              <LeaderBoard
-                width="700px"
-                height="52px"
-                justifyContent="center"
-              />
-              <BalanceBoard width="180px" height="52px" />
-            </Flex>
-          )}
-          {!isHeatMapMode && (
-            <GameMenu
-              position="absolute"
-              width="60px"
-              height="204px"
-              right={`${outterSharedMargin}px`}
-              top={`${156 + NAVBAR_HIGHT}px`}
-              p="0px"
-              m="0px"
-            />
-          )}
-          {!isHeatMapMode && (
-            <Button
-              width="146px"
-              height="146px"
-              position="absolute"
-              bottom={`${outterSharedMargin + 146 + 30}px`}
-              right={`${outterSharedMargin}px`}
-              onClick={onClickBuyLand}
-              _hover={{
-                background:
-                  "0px 25px 60px -15px rgba(3, 3, 3, 0.20), 0px 25px 60px -15px rgba(3, 3, 3, 0.12), 0px 0px 7px 0px #222",
-                boxShadow:
-                  "0px 25px 60px -15px rgba(16, 24, 40, 0.20), 0px 25px 60px -15px rgba(16, 24, 40, 0.12), 0px 0px 7px 0px #222",
-                linearGradient: "(270deg, #FFF -26.8%, #000 30.45%)",
-              }}
-              _active={{
-                background: "linear-gradient(270deg, #FFF -26.8%, #000 30.45%)",
-                boxShadow:
-                  "0px 25px 60px -15px rgba(16, 24, 40, 0.20), 0px 25px 60px -15px rgba(16, 24, 40, 0.12), -6px 1px 0px 0px rgba(0, 0, 0, 0.40) inset",
-                transform: "scale(0.98)",
-              }}
-              boxShadow="0px 25px 60px -15px rgba(16, 24, 40, 0.20), 0px 25px 60px -15px rgba(16, 24, 40, 0.12)"
-              background="linear-gradient(270deg, #FFF -26.8%, #000 30.45%)"
-              borderRadius="16.22px"
-            >
-              <Flex direction="column" alignItems="center">
-                <Image src={dice}></Image>
-                <Text
-                  color="#FFFFFF"
-                  textAlign="center"
-                  fontFamily="Inter"
-                  fontSize="26px"
-                  fontStyle="normal"
-                  fontWeight="500"
-                  lineHeight="35.689px"
-                  letterSpacing="-0.72px"
-                >
-                  Buy Land!
-                </Text>
-              </Flex>
-            </Button>
-          )}
-          {!isHeatMapMode && (
-            <Button
-              width="146px"
-              height="146px"
-              position="absolute"
-              bottom={`${outterSharedMargin}px`}
-              right={`${outterSharedMargin}px`}
-              onClick={onClickMove}
-              _hover={{
-                background:
-                  "0px 25px 60px -15px rgba(3, 3, 3, 0.20), 0px 25px 60px -15px rgba(3, 3, 3, 0.12), 0px 0px 7px 0px #222",
-                boxShadow:
-                  "0px 25px 60px -15px rgba(16, 24, 40, 0.20), 0px 25px 60px -15px rgba(16, 24, 40, 0.12), 0px 0px 7px 0px #222",
-                linearGradient: "(270deg, #FFF -26.8%, #000 30.45%)",
-              }}
-              _active={{
-                background: "linear-gradient(270deg, #FFF -26.8%, #000 30.45%)",
-                boxShadow:
-                  "0px 25px 60px -15px rgba(16, 24, 40, 0.20), 0px 25px 60px -15px rgba(16, 24, 40, 0.12), -6px 1px 0px 0px rgba(0, 0, 0, 0.40) inset",
-                transform: "scale(0.98)",
-              }}
-              boxShadow="0px 25px 60px -15px rgba(16, 24, 40, 0.20), 0px 25px 60px -15px rgba(16, 24, 40, 0.12)"
-              background="linear-gradient(270deg, #FFF -26.8%, #000 30.45%)"
-              borderRadius="16.22px"
-            >
-              <Flex direction="column" alignItems="center">
-                <Image src={dice}></Image>
-                <Text
-                  color="#FFFFFF"
-                  textAlign="center"
-                  fontFamily="Inter"
-                  fontSize="36px"
-                  fontStyle="normal"
-                  fontWeight="500"
-                  lineHeight="35.689px"
-                  letterSpacing="-0.72px"
-                >
-                  GO!
-                </Text>
-              </Flex>
-            </Button>
-          )}
-          <Switch
-            position="absolute"
-            left={`${outterSharedMargin}px`}
-            bottom={`${outterSharedMargin}px`}
-            size="lg"
-            colorScheme="red"
-            onChange={onHeatMapSwitchClick}
-          />
-        </Box>
       )}
     </ConnectModalProvider>
   );
