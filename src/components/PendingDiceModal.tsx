b274b5c2492d7d1a6e154a54e0c593e699acd0cf
moojing <starts789@gmail.com>
2024-02-28 22:55:54 +0800

    fix: type error.

--- src/components/PendingDiceModal.tsx
+++ src/components/PendingDiceModal.tsx
@@ -1,37 +1,29 @@
+import { Modal, ModalOverlay, ModalContent, ModalBody } from "@chakra-ui/react";
+import DicePendingImage from "src/assets/roll-dice-pending.svg?react";
 
-import { 
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  ModalFooter,
-  Button
- } from "@chakra-ui/react";
- import DicePendingImage from 'src/assets/roll-dice-pending.svg?react'
-
-export default function PendingDiceModal(
-  {isOpen, onClose}: {isOpen: boolean, onClose: () => void}
-){
-
-  return  <Modal isOpen={isOpen} onClose={onClose} isCentered>
-  <ModalOverlay />
-  <ModalContent
-  width="564px"
-  height="auto"
-  p="16px"
-  pt="40px"
-  maxW="564px"
-  borderRadius="0px"
-  bg="#25291B"
-   >
-    
-    <ModalBody p="0" >
-      <DicePendingImage width="100%"/>
-    </ModalBody>
-     
-  </ModalContent>
-</Modal>
-
+export default function PendingDiceModal({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent
+        width="564px"
+        height="auto"
+        p="16px"
+        pt="40px"
+        maxW="564px"
+        borderRadius="0px"
+        bg="#25291B"
+      >
+        <ModalBody p="0">
+          <DicePendingImage width="100%" />
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
 }
