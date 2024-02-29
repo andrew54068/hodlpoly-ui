d80b143fcb3dbd852f3b1561a5f03a182b9b6d3b
moojing <starts789@gmail.com>
2024-02-29 16:51:59 +0800

    feat: add dice rolling animation.

--- src/components/PendingDiceModal.tsx
+++ src/components/PendingDiceModal.tsx
@@ -1,5 +1,51 @@
 import { Modal, ModalOverlay, ModalContent, ModalBody } from "@chakra-ui/react";
-import DicePendingImage from "src/assets/roll-dice-pending.svg?react";
+import { Box, Flex } from "@chakra-ui/react";
+import Dice from "src/assets/dice.svg?react";
+import { motion } from "framer-motion";
+
+const DiceRoll = () => {
+  // for infinity animation effect
+  const extendedDiceNumbers = [5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4];
+  const visibleNumbersHeight = extendedDiceNumbers.length * 100;
+  const animationSettings = {
+    initial: { y: 0 },
+    animate: { y: -visibleNumbersHeight },
+    transition: {
+      duration: 1,
+      ease: "linear",
+      repeat: Infinity,
+    },
+  };
+
+  return (
+    <Box
+      overflow="hidden"
+      height="100px"
+      width="100px"
+      position="relative"
+      ml="24px"
+    >
+      <motion.div {...animationSettings}>
+        {extendedDiceNumbers.map((number, index) => (
+          <Box
+            key={index}
+            height="100px"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            fontSize="128px"
+            w="62px"
+            mb="24px"
+            color="#FCFC54"
+            textShadow="0px 0px 10px #FCFC54"
+          >
+            {number}
+          </Box>
+        ))}
+      </motion.div>
+    </Box>
+  );
+};
 
 export default function PendingDiceModal({
   isOpen,
@@ -13,15 +59,25 @@ export default function PendingDiceModal({
       <ModalOverlay />
       <ModalContent
         width="564px"
-        height="auto"
+        height="380px"
         p="16px"
         pt="40px"
         maxW="564px"
         borderRadius="0px"
-        bg="#25291B"
+        bg="gray.oliver.dark"
       >
-        <ModalBody p="0">
-          <DicePendingImage width="100%" />
+        <ModalBody
+          p="24px"
+          w="100%"
+          bg="background.dark"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Flex justify="center" align="center">
+            <Dice width="158px" height="150px" />
+            <DiceRoll />
+          </Flex>
         </ModalBody>
       </ModalContent>
     </Modal>
