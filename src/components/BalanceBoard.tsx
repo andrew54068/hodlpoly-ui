f9a8cd9a498200be8c894505b95bb7a3139e8d23
moojing <starts789@gmail.com>
2024-02-29 15:10:24 +0800

    feat: add revenue modal.

--- src/components/BalanceBoard.tsx
+++ src/components/BalanceBoard.tsx
@@ -1,11 +1,15 @@
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { useDisclosure, Box, Flex, Image, Text } from "@chakra-ui/react";
 import useUserFomopolyData from "src/hooks/useUserFomopolyData";
 import logo from "src/assets/logo.png";
 import { formatEther } from "viem";
+import RevenueModal from "./RevenueModal";
 
 export const BalanceBoard = ({ ...rest }: any) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { playerClaimableReward } = useUserFomopolyData();
-
+  const formattedReward = parseFloat(
+    formatEther(playerClaimableReward)
+  ).toFixed(8);
   return (
     <Box
       borderRadius="12px"
@@ -13,9 +17,16 @@ export const BalanceBoard = ({ ...rest }: any) => {
       background="var(--Generic-White, #FFF)"
       boxShadow="0px 2px 6px 0px rgba(16, 24, 40, 0.06)"
       p="8px"
+      cursor={"pointer"}
+      onClick={onOpen}
       {...rest}
     >
-      <Flex height="100%" justifyContent="space-between" alignItems="center">
+      <Flex
+        height="100%"
+        justifyContent="space-between"
+        alignItems="center"
+        minWidth="160px"
+      >
         <Image src={logo}></Image>
         <Text
           color="var(--Neutral-500, #6B7280)"
@@ -24,9 +35,15 @@ export const BalanceBoard = ({ ...rest }: any) => {
           fontWeight="600"
           lineHeight="28px"
         >
-          {formatEther(playerClaimableReward)}
+          {formattedReward}
         </Text>
       </Flex>
+      <RevenueModal
+        isOpen={isOpen}
+        onClose={onClose}
+        revenueETH="0.12345"
+        claimableBalanceETH="0.652123"
+      />
     </Box>
   );
 };
