96fde436b1cdd0d1268a598b61f23e9dab286497
moojing <starts789@gmail.com>
2024-03-06 15:56:54 +0800

    chore: update fomopoly contract address.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 const isMainnet = import.meta.env.VITE_NETWORK === 'mainnet'
-export const FOMOPOLY_PROXY_ADDRESS = isMainnet ? '0x' : '0x3f4D8B1124Ea549c8E9caD85E7E663084BD86F80';
-export const FMP_PROXY_ADDRESS = isMainnet ? '0x' : '0xe17D8598CEa98F2f5281998d58033bd615fbc07C'
+export const FOMOPOLY_PROXY_ADDRESS = isMainnet ? '0x' : '0x68E331828dE32Fc9C1E921F160D31127850a991A';
+export const FMP_PROXY_ADDRESS = isMainnet ? '0x' : '0xD47Ee70312688eDCC4eBAb1638aB8c07e1C7e4c2'
