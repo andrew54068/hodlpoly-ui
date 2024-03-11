0f0ede058467ceba05270fd7f725130fca8222a1
moojing <starts789@gmail.com>
2024-03-12 00:45:01 +0800

    feat: call getLand function in BuyLandModal.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 const isMainnet = import.meta.env.VITE_NETWORK === 'mainnet'
-export const FOMOPOLY_PROXY_ADDRESS = isMainnet ? '0x' : '0x68E331828dE32Fc9C1E921F160D31127850a991A';
+export const FOMOPOLY_PROXY_ADDRESS = isMainnet ? '0x' : '0x660c0B1E8D56E3Be50e1b5e328EAF18DE54DC464';
 export const FMP_PROXY_ADDRESS = isMainnet ? '0x' : '0xD47Ee70312688eDCC4eBAb1638aB8c07e1C7e4c2'
